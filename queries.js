3a660b806b0aa7aa13cf2b78db8b3f1f66dcfa72
jamescodegaard <jamescodegaard@gmail.com>
2020-12-29 15:42:10 -0600

    updates user_id CRUD actions

--- queries.js
+++ queries.js
@@ -19,9 +19,9 @@ const getUsers = (request, response) => {
 };
 
 const getUserById = (request, response) => {
-  const id = parseInt(request.params.id);
+  const user_id = parseInt(request.params.id);
 
-  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+  pool.query("SELECT * FROM users WHERE user_id = $1", [user_id], (error, results) => {
     if (error) {
       throw error;
     }
@@ -33,43 +33,43 @@ const createUser = (request, response) => {
   const { name, email } = request.body;
 
   pool.query(
-    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING user_id",
     [name, email],
     (error, result) => {
       if (error) {
         throw error;
       }
       console.log(result);
-      response.status(201).send(`User added with ID: ${result.rows[0].id}`);
+      response.status(201).send(`User added with ID: ${result.rows[0].user_id}`);
       // console.log(response);
     }
   );
 };
 
 const updateUser = (request, response) => {
-  const id = parseInt(request.params.id);
+  const user_id = parseInt(request.params.id);
   const { name, email } = request.body;
 
   pool.query(
-    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
-    [name, email, id],
+    "UPDATE users SET name = $1, email = $2 WHERE user_id = $3",
+    [name, email, user_id],
     (error, result) => {
       if (error) {
         throw error;
       }
-      response.status(200).send(`User modified with ID: ${id}`);
+      response.status(200).send(`User modified with ID: ${user_id}`);
     }
   );
 };
 
 const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id);
+  const user_id = parseInt(request.params.id);
 
-  pool.query("DELETE FROM users WHERE id = $1", [id], (error, result) => {
+  pool.query("DELETE FROM users WHERE user_id = $1", [user_id], (error, result) => {
     if (error) {
       throw error;
     }
-    response.status(200).send(`User deleted with ID: ${id}`);
+    response.status(200).send(`User deleted with ID: ${user_id}`);
   });
 };
 
@@ -92,7 +92,7 @@ const getProfessorById = (request, response) => {
   const id = parseInt(request.params.id);
 
   pool.query(
-    "SELECT * FROM professors WHERE id = $1",
+    "SELECT * FROM professors WHERE professr_id = $1",
     [id],
     (error, result) => {
       if (error) {
